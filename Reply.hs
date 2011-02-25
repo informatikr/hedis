1ae3f0f3a503190e28224d1c7a9cf4823a4c2082
Falko Peters <falko.peters@googlemail.com>
2011-02-26 00:20:30 +0100

    enable multi-bulk replies to recursively contain other multi-bulks. Used by MULTI/EXEC.

--- Reply.hs
+++ Reply.hs
@@ -2,19 +2,19 @@
 module Reply (Reply(..), parseReply) where
 
 import Prelude hiding (error, take)
+import Control.Applicative
+import Control.Concurrent
 import Data.Attoparsec.Char8
 import qualified Data.Attoparsec.Lazy as P
 import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy.Char8 as L
 
-import System.IO.Unsafe
-import Control.Concurrent
 
 data Reply = SingleLine S.ByteString
            | Error S.ByteString
            | Integer Integer
            | Bulk (Maybe S.ByteString)
-           | MultiBulk (Maybe [Maybe S.ByteString])
+           | MultiBulk (Maybe [Reply])
            | ConnectionLost
          deriving (Show)
 
@@ -43,28 +43,26 @@ integer :: Parser Reply
 integer = fmap Integer $ ':' `prefixing` signed decimal
 
 bulk :: Parser Reply
-bulk = fmap Bulk bulk'
+bulk = fmap Bulk $ do    
+    len <- '$' `prefixing` signed decimal
+    if len < 0
+        then return Nothing
+        else fmap Just $ beforeCRLF (P.take len)
 
 multiBulk :: Parser Reply
 multiBulk = fmap MultiBulk $ do
         len <- '*' `prefixing` signed decimal
         if len < 0
             then return Nothing
-            else fmap Just $ count len bulk'
+            else fmap Just $ count len (bulk <|> multiBulk)
 
 shutdown :: Parser Reply
 shutdown = P.endOfInput >> return ConnectionLost
 
+
 ------------------------------------------------------------------------------
 -- Helpers & Combinators
 --
-bulk' :: Parser (Maybe S.ByteString)
-bulk' = do    
-    len <- '$' `prefixing` signed decimal
-    if len < 0
-        then return Nothing
-        else fmap Just $ beforeCRLF (P.take len)
-
 prefixing :: Char -> Parser a -> Parser a
 c `prefixing` a = char c >> beforeCRLF a
 
