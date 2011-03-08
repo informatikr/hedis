90e2b0f482a89fa8fce384d3e241e8f9740c723c
Falko Peters <falko.peters@googlemail.com>
2011-03-08 20:16:23 +0100

    generalize th-command definitions.

--- src/Database/Redis.hs
+++ src/Database/Redis.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
 module Database.Redis (
     module Database.Redis.Internal,
@@ -19,24 +19,11 @@ import Database.Redis.Types
 ------------------------------------------------------------------------------
 -- Redis commands
 --
-cmd "exists" "key" bool
 
-incr :: RedisInt a => ByteString -> Redis (Maybe a)
-incr key = decodeInt <$> sendRequest ["INCR", key]
-
-hgetall :: RedisHash a => ByteString -> Redis (Maybe a)
-hgetall key = decodeHash <$> sendRequest ["HGETALL", key]
-
-lrange :: RedisList a =>
-          ByteString -> ByteString -> ByteString -> Redis (Maybe a)
-lrange key start stop =
-    decodeList <$> sendRequest ["LRANGE", key, start, stop]
-
-sunion :: RedisSet a => [ByteString] -> Redis (Maybe a)
-sunion keys = decodeSet <$> sendRequest ("SUNION" : keys)
-
-ping :: RedisStatus a => Redis (Maybe a)
-ping = decodeStatus <$> sendRequest ["PING"]
-
-rename :: RedisStatus a => ByteString -> ByteString -> Redis (Maybe a)
-rename k k' = decodeStatus <$> sendRequest ["RENAME", k, k']
+cmd status "ping" ""
+cmd bool "exists" "key"
+cmd int "incr" "key"
+cmd hash "hgetall" "key"
+cmd list "lrange" "key start stop"
+cmd status "rename" "k k'"
+cmdVar set "sunion" "" "keys"
--- src/Database/Redis/CommandTemplates.hs
+++ src/Database/Redis/CommandTemplates.hs
@@ -1,35 +1,101 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Database.Redis.CommandTemplates (
+    ReturnType,
+    status,
+    bool,
+    int,
+    list,
+    set,
+    hash,
     cmd,
-    bool
+    cmdVar
 ) where
 
-
+import Control.Monad
 import Data.Char
+import Data.Maybe
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
+import Language.Haskell.TH.Lib
 
 
 ------------------------------------------------------------------------------
--- Redis type names
+-- Public interface
 --
-bool :: String
-bool = "Bool"
+
+newtype ReturnType = Typ String
+
+status, bool, int, list, hash, set :: ReturnType
+status = Typ "Status"
+bool   = Typ "Bool"
+int    = Typ "Int"
+list   = Typ "List"
+hash   = Typ "Hash"
+set    = Typ "Set"
+
+cmd :: ReturnType -> String -> String -> Q [Dec]
+cmd typ name args = defCmd typ name args Nothing
+
+cmdVar :: ReturnType -> String -> String -> String -> Q [Dec]
+cmdVar typ name args varArgs = defCmd typ name args (Just varArgs)
+
 
 ------------------------------------------------------------------------------
--- Command templates
+-- Command template
 --
-cmd :: String -> String -> String -> Q [Dec]
-cmd name arg returnType = sequenceQ
-    [ funD (mkName name)
-        [ clause
-            [varP $ mkName arg]
-            (normalB $ infixApp decode (varE . mkName $ "<$>") sendReq)
-            []
-        ]
-    ]
+defCmd :: ReturnType -> String -> String -> Maybe String -> Q [Dec]
+defCmd (Typ typ) name args varArgs =
+    forM [sig, fun] $ \dec -> dec typ name (words args) varArgs
+    
+sig :: String -> String -> [String] -> Maybe String -> DecQ
+sig typ name args varArgs = sigD (mkName name) $
+    forallT [plainTV (mkName "a")] (redisTypeClass typ) $
+        appArrowsT (argsT ++ varArgT ++ returnT)
   where
-    decode  = varE . mkName $ "decode" ++ returnType
-    sendReq = appE (varE . mkName $ "sendRequest") argList
-    argList = listE [ litE . stringL . map toUpper $ name
-                    , varE $ mkName arg
-                    ]
+    argsT   = replicate (length args) byteStringT
+    varArgT = maybe [] (const [listT `appT` byteStringT]) varArgs
+    returnT = [nameT "Redis" `appT` (nameT "Maybe" `appT` (nameT "a"))]
+
+
+fun :: String -> String -> [String] -> Maybe String -> DecQ
+fun typ name args varArgs =
+    funD (mkName name) [clause funArgs (normalB funBody) []]
+  where
+    funArgs  = map nameP args ++ maybeToList (fmap nameP varArgs)
+    funBody  = resultMapper typ (sendReq argList)
+    sendReq  = appE (nameE "sendRequest")
+    argList  = maybe argList'
+                (infixApp argList' (nameE "++") . nameE)
+                varArgs
+    argList' = listE $ (cmdE name) : (map nameE args)    
+
+
+------------------------------------------------------------------------------
+-- Helpers
+--
+appArrowsT :: [TypeQ] -> TypeQ
+appArrowsT = foldr1 $ appT . appT arrowT
+
+resultMapper :: String -> ExpQ -> ExpQ
+resultMapper typ = infixApp (nameE $ "decode" ++ typ) (nameE "<$>")
+
+redisTypeClass :: String -> Q [Pred]
+redisTypeClass typ = sequence [classP (mkName $ "Redis" ++ typ) [nameT "a"]]
+
+nameP :: String -> PatQ
+nameP = varP . mkName
+
+nameE :: String -> ExpQ
+nameE = varE . mkName
+
+cmdE :: String -> ExpQ
+cmdE = litE . stringL . map toUpper
+
+nameT :: String -> TypeQ
+nameT = f `ap` mkName
+  where
+    f "" = error "type-name must not be empty"
+    f (c:_) | isUpper c = conT
+            | otherwise = varT
+
+byteStringT :: TypeQ
+byteStringT = nameT "ByteString"
