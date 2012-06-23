4c24c1aba8bab1fd48ad2a4f683180c4ebb9e95d
Falko Peters <falko.peters@googlemail.com>
2012-06-23 18:51:22 +0200

    Reduce test code.
    It is sufficient to test that commands do not return Redis errors. We
    do not need to test Redis itself. A smaller test codebase will make it
    easier to change return types or the container types determined by
    RedisCtx.

--- test/Test.hs
+++ test/Test.hs
@@ -45,7 +45,7 @@ assert = liftIO . Test.assert
 --
 tests :: [Test]
 tests = concat
-    [ testsMisc, testsKeys, testsStrings, testsHashes, testsLists, testsZSets
+    [ testsMisc, testsKeys, [testStrings], [testHashes], testsLists, testsZSets
     , [testPubSub], [testTransaction], [testScripting], testsConnection
     , testsServer, [testQuit]
     ]
@@ -93,31 +93,24 @@ testPipelining = testCase "pipelining" $ do
 -- Keys
 --
 testsKeys :: [Test]
-testsKeys =
-    [ testDel, testExists, testExpire, testExpireAt, testKeys, testMove
-    , testPersist, testRandomkey, testRename, testRenamenx, testSort
-    , testTtl, testGetType, testObject
-    ]
+testsKeys = [ testKeys, testExpireAt, testSort, testGetType, testObject ]
 
-testDel :: Test
-testDel = testCase "del" $ do
-    set "key" "value" >>=? Ok
-    get "key"         >>=? Just "value"
-    del ["key"]       >>=? 1
-    get "key"         >>=? Nothing
-
-testExists :: Test
-testExists = testCase "exists" $ do
-    exists "key"      >>=? False
-    set "key" "value" >>=? Ok
-    exists "key"      >>=? True
-
-testExpire :: Test
-testExpire = testCase "expire" $ do
-    set "key" "value"  >>=? Ok
-    expire "key" 1     >>=? True
-    expire "notAKey" 1 >>=? False
-    ttl "key"          >>=? 1
+testKeys :: Test
+testKeys = testCase "keys" $ do
+    set "key" "value"     >>=? Ok
+    get "key"             >>=? Just "value"
+    exists "key"          >>=? True
+    keys "*"              >>=? ["key"]
+    randomkey             >>=? Just "key"
+    move "key" 13         >>=? True
+    select 13             >>=? Ok
+    expire "key" 1        >>=? True
+    ttl "key"             >>=? 1
+    persist "key"         >>=? True
+    rename "key" "key'"   >>=? Ok
+    renamenx "key'" "key" >>=? True
+    del ["key"]           >>=? 1
+    select 0              >>=? Ok
     
 testExpireAt :: Test
 testExpireAt = testCase "expireat" $ do
@@ -128,52 +121,6 @@ testExpireAt = testCase "expireat" $ do
     expireat "notAKey" expiry >>=? False
     ttl "key"                 >>=? 1
 
-testKeys :: Test
-testKeys = testCase "keys" $ do
-    keys "key*"      >>=? []
-    set "key1" "value" >>=? Ok
-    set "key2" "value" >>=? Ok
-    Right ks <- keys "key*"
-    assert $ length ks == 2
-    assert $ elem "key1" ks
-    assert $ elem "key2" ks
-
-testMove :: Test
-testMove = testCase "move" $ do
-    set "key" "value" >>=? Ok
-    move "key" 13     >>=? True
-    get "key"         >>=? Nothing
-    select 13         >>=? Ok
-    get "key"         >>=? Just "value"
-
-testPersist :: Test
-testPersist = testCase "persist" $ do
-    set "key" "value" >>=? Ok
-    expire "key" 1    >>=? True
-    ttl "key"         >>=? 1
-    persist "key"     >>=? True
-    ttl "key"         >>=? (-1)
-
-testRandomkey :: Test
-testRandomkey = testCase "randomkey" $ do
-    set "k1" "value" >>=? Ok
-    set "k2" "value" >>=? Ok
-    Right (Just k) <- randomkey
-    assert $ k `elem` ["k1", "k2"]
-
-testRename :: Test
-testRename = testCase "rename" $ do
-    set "k1" "value" >>=? Ok
-    rename "k1" "k2" >>=? Ok
-    get "k1"         >>=? Nothing
-    get "k2"         >>=? Just ("value" )
-
-testRenamenx :: Test
-testRenamenx = testCase "renamenx" $ do
-    set "k1" "value"   >>=? Ok
-    set "k2" "value"   >>=? Ok
-    renamenx "k1" "k2" >>=? False
-    renamenx "k1" "k3" >>=? True
 
 testSort :: Test
 testSort = testCase "sort" $ do
@@ -193,14 +140,6 @@ testSort = testCase "sort" $ do
                                , sortGet   = ["#", "object_*"] }
     sort "ids" opts >>=? ["2", "bar", "1", "foo"]
     
-    
-testTtl :: Test
-testTtl = testCase "ttl" $ do
-    set "key" "value" >>=? Ok
-    ttl "notAKey"     >>=? (-1)
-    ttl "key"         >>=? (-1)
-    expire "key" 42   >>=? True
-    ttl "key"         >>=? 42
 
 testGetType :: Test
 testGetType = testCase "getType" $ do
@@ -227,235 +166,81 @@ testObject = testCase "object" $ do
 ------------------------------------------------------------------------------
 -- Strings
 --
-testsStrings :: [Test]
-testsStrings =
-    [ testAppend, testDecr, testDecrby, testGetbit, testGetrange, testGetset
-    , testIncr, testIncrby, testMget, testMset, testMsetnx, testSetbit
-    , testSetex, testSetnx, testSetrange, testStrlen, testSetAndGet
-    ]
-
-testAppend :: Test
-testAppend = testCase "append" $ do
-    set "key" "hello"    >>=? Ok
-    append "key" "world" >>=? 10
-    get "key"            >>=? Just "helloworld"
-
-testDecr :: Test
-testDecr = testCase "decr" $ do
-    set "key" "42" >>=? Ok
-    decr "key"     >>=? 41
-
-testDecrby :: Test
-testDecrby = testCase "decrby" $ do
-    set "key" "42"  >>=? Ok
-    decrby "key" 2  >>=? 40
-
-testGetbit :: Test
-testGetbit = testCase "getbit" $ getbit "key" 42 >>=? 0
-
-testGetrange :: Test
-testGetrange = testCase "getrange" $ do
-    set "key" "value"     >>=? Ok
-    getrange "key" 1 (-2) >>=? "alu"
-
-testGetset :: Test
-testGetset = testCase "getset" $ do
-    getset "key" "v1" >>=? Nothing
-    getset "key" "v2" >>=? Just "v1"
-
-testIncr :: Test
-testIncr = testCase "incr" $ do
-    set "key" "42" >>=? Ok
-    incr "key"     >>=? 43
-
-testIncrby :: Test
-testIncrby = testCase "incrby" $ do
-    set "key" "40" >>=? Ok
-    incrby "key" 2 >>=? 42
-
-testMget :: Test
-testMget = testCase "mget" $ do
-    set "k1" "v1"               >>=? Ok
-    set "k2" "v2"               >>=? Ok
-    mget ["k1","k2","notAKey" ] >>=? [Just "v1", Just "v2", Nothing]
-
-testMset :: Test
-testMset = testCase "mset" $ do
-    mset [("k1","v1"), ("k2","v2")] >>=? Ok
-    get "k1"                        >>=? Just "v1"
-    get "k2"                        >>=? Just "v2"
-
-testMsetnx :: Test
-testMsetnx = testCase "msetnx" $ do
-    msetnx [("k1","v1"), ("k2","v2")] >>=? True
+testStrings :: Test
+testStrings = testCase "strings" $ do
+    setnx "key" "value"               >>=? True
+    getset "key" "hello"              >>=? Just "value"
+    append "key" "world"              >>=? 10
+    strlen "key"                      >>=? 10
+    setrange "key" 0 "hello"          >>=? 10
+    getrange "key" 0 4                >>=? "hello"
+    mset [("k1","v1"), ("k2","v2")]   >>=? Ok
     msetnx [("k1","v1"), ("k2","v2")] >>=? False
-
-testSetbit :: Test
-testSetbit = testCase "setbit" $ do
-    setbit "key" 42 "1" >>=? 0
-    setbit "key" 42 "0" >>=? 1
-    
-testSetex :: Test
-testSetex = testCase "setex" $ do
-    setex "key" 1 "value" >>=? Ok
-    ttl "key"             >>=? 1
-
-testSetnx :: Test
-testSetnx = testCase "setnx" $ do
-    setnx "key" "v1" >>=? True
-    setnx "key" "v2" >>=? False
-
-testSetrange :: Test
-testSetrange = testCase "setrange" $ do
-    set "key" "value"      >>=? Ok
-    setrange "key" 1 "ers" >>=? 5
-    get "key"              >>=? Just "verse"
-
-testStrlen :: Test
-testStrlen = testCase "strlen" $ do
-    set "key" "value" >>=? Ok
-    strlen "key"      >>=? 5
-
-testSetAndGet :: Test
-testSetAndGet = testCase "set/get" $ do
-    get "key"         >>=? Nothing
-    set "key" "value" >>=? Ok
-    get "key"         >>=? Just "value"
-
+    mget ["key"]                      >>=? [Just "helloworld"]
+    setex "key" 1 "42"                >>=? Ok
+    decr "key"                        >>=? 41
+    decrby "key" 1                    >>=? 40
+    incr "key"                        >>=? 41
+    incrby "key" 1                    >>=? 42
+    setbit "key" 42 "1"               >>=? 0
+    getbit "key" 42                   >>=? 1
 
 ------------------------------------------------------------------------------
 -- Hashes
 --
-testsHashes :: [Test]
-testsHashes =
-    [ testHdel, testHexists,testHget, testHgetall, testHincrby, testHkeys
-    , testHlen, testHmget, testHmset, testHset, testHsetnx, testHvals
-    ]
-
-testHdel :: Test
-testHdel = testCase "hdel" $ do
-    hdel "key" ["field"]       >>=? False
-    hset "key" "field" "value" >>=? True
-    hdel "key" ["field"]       >>=? True
-
-testHexists :: Test
-testHexists = testCase "hexists" $ do
-    hexists "key" "field"      >>=? False
-    hset "key" "field" "value" >>=? True
-    hexists "key" "field"      >>=? True
-
-testHget :: Test
-testHget = testCase "hget" $ do
-    hget "key" "field"         >>=? Nothing
-    hset "key" "field" "value" >>=? True
-    hget "key" "field"         >>=? Just "value"
-
-testHgetall :: Test
-testHgetall = testCase "hgetall" $ do
-    hgetall "key"                         >>=? []
-    hmset "key" [("f1","v1"),("f2","v2")] >>=? Ok
-    hgetall "key"                         >>=? [("f1","v1"), ("f2","v2")]
-    
-testHincrby :: Test
-testHincrby = testCase "hincrby" $ do
-    hset "key" "field" "40" >>=? True
-    hincrby "key" "field" 2 >>=? 42
-
-testHkeys :: Test
-testHkeys = testCase "hkeys" $ do
-    hset "key" "field" "value" >>=? True
-    hkeys "key"                >>=? ["field"]
-
-testHlen :: Test
-testHlen = testCase "hlen" $ do
-    hlen "key"                 >>=? 0
-    hset "key" "field" "value" >>=? True
-    hlen "key"                 >>=? 1
-
-testHmget :: Test
-testHmget = testCase "hmget" $ do
-    hmset "key" [("f1","v1"), ("f2","v2")] >>=? Ok
-    hmget "key" ["f1", "f2", "nofield" ]   >>=? [Just "v1", Just "v2", Nothing]
-
-testHmset :: Test
-testHmset = testCase "hmset" $ do
-    hmset "key" [("f1","v1"), ("f2","v2")] >>=? Ok
-
-testHset :: Test
-testHset = testCase "hset" $ do
-    hset "key" "field" "value" >>=? True
-    hset "key" "field" "value" >>=? False
-
-testHsetnx :: Test
-testHsetnx = testCase "hsetnx" $ do
-    hsetnx "key" "field" "value" >>=? True
+testHashes :: Test
+testHashes = testCase "hashes" $ do
+    hset "key" "field" "value"   >>=? True
     hsetnx "key" "field" "value" >>=? False
-
-testHvals :: Test
-testHvals = testCase "hvals" $ do
-    hset "key" "field" "value" >>=? True
-    hvals "key"                >>=? ["value"]
-
+    hexists "key" "field"        >>=? True
+    hlen "key"                   >>=? 1
+    hget "key" "field"           >>=? Just "value"
+    hmget "key" ["field", "-"]   >>=? [Just "value", Nothing]
+    hgetall "key"                >>=? [("field","value")]
+    hkeys "key"                  >>=? ["field"]
+    hvals "key"                  >>=? ["value"]
+    hdel "key" ["field"]         >>=? True
+    hmset "key" [("field","40")] >>=? Ok
+    hincrby "key" "field" 2      >>=? 42
 
 ------------------------------------------------------------------------------
 -- Lists
 --
 testsLists :: [Test]
 testsLists =
-    [testBlpop, testBrpoplpush, testLpop, testLinsert, testLpushx, testLset]
+    [testLists, testBpop]
 
-testBlpop :: Test
-testBlpop = testCase "blpop/brpop" $ do
+testLists :: Test
+testLists = testCase "lists" $ do
+    lpushx "notAKey" "-"          >>=? 0
+    rpushx "notAKey" "-"          >>=? 0
+    lpush "key" ["value"]         >>=? 1
+    lpop "key"                    >>=? Just "value"
+    rpush "key" ["value"]         >>=? 1
+    rpop "key"                    >>=? Just "value"
+    rpush "key" ["v2"]            >>=? 1
+    linsertBefore "key" "v2" "v1" >>=? 2
+    linsertAfter "key" "v2" "v3"  >>=? 3    
+    lindex "key" 0                >>=? Just "v1"
+    lrange "key" 0 (-1)           >>=? ["v1", "v2", "v3"]
+    lset "key" 1 "v2"             >>=? Ok
+    lrem "key" 0 "v2"             >>=? 1
+    llen "key"                    >>=? 2
+    ltrim "key" 0 1               >>=? Ok
+
+testBpop :: Test
+testBpop = testCase "blocking push/pop" $ do
     lpush "key" ["v3","v2","v1"] >>=? 3
-    blpop ["notAKey","key"] 1    >>=? Just ("key","v1")
-    brpop ["notAKey","key"] 1    >>=? Just ("key","v3")
+    blpop ["key"] 1              >>=? Just ("key","v1")
+    brpop ["key"] 1              >>=? Just ("key","v3")
     -- run into timeout
     blpop ["notAKey"] 1          >>=? Nothing
-
-testBrpoplpush :: Test
-testBrpoplpush = testCase "brpoplpush/rpoplpush" $ do
-    rpush "k1" ["v1","v2"]      >>=? 2
-    brpoplpush "k1" "k2" 1      >>=? Just "v2"
-    rpoplpush "k1" "k2"         >>=? Just "v1"
-    rpoplpush "notAKey" "k2"    >>=? Nothing
-    llen "k2"                   >>=? 2
-    llen "k1"                   >>=? 0
+    rpush "k1" ["v1","v2"]       >>=? 2
+    brpoplpush "k1" "k2" 1       >>=? Just "v2"
+    rpoplpush "k1" "k2"          >>=? Just "v1"
     -- run into timeout
-    brpoplpush "notAKey" "k2" 1 >>=? Nothing
-
-testLpop :: Test
-testLpop = testCase "lpop/rpop" $ do
-    lpush "key" ["v3","v2","v1"] >>=? 3
-    lpop "key"                   >>=? Just "v1"
-    llen "key"                   >>=? 2
-    rpop "key"                   >>=? Just "v3"
-
-testLinsert :: Test
-testLinsert = testCase "linsert" $ do
-    rpush "key" ["v2"]                 >>=? 1
-    linsertBefore "key" "v2" "v1"      >>=? 2
-    linsertBefore "key" "notAVal" "v3" >>=? (-1)
-    linsertAfter "key" "v2" "v3"       >>=? 3    
-    linsertAfter "key" "notAVal" "v3"  >>=? (-1)
-    lindex "key" 0                     >>=? Just "v1"
-    lindex "key" 2                     >>=? Just "v3"
-
-testLpushx :: Test
-testLpushx = testCase "lpushx/rpushx" $ do
-    lpushx "notAKey" "v1" >>=? 0
-    lpush "key" ["v2"]    >>=? 1
-    lpushx "key" "v1"     >>=? 2
-    rpushx "key" "v3"     >>=? 3
-
-testLset :: Test
-testLset = testCase "lset/lrem/ltrim" $ do
-    lpush "key" ["v3","v2","v2","v1","v1"] >>=? 5
-    lset "key" 1 "v2"                      >>=? Ok
-    lrem "key" 2 "v2"                      >>=? 2
-    llen "key"                             >>=? 3
-    ltrim "key" 0 1                        >>=? Ok
-    lrange "key" 0 1                       >>=? ["v1", "v2"]
-
+    brpoplpush "notAKey" "k2" 1  >>=? Nothing    
+    
 ------------------------------------------------------------------------------
 -- Sets
 --
@@ -464,38 +249,23 @@ testLset = testCase "lset/lrem/ltrim" $ do
 -- Sorted Sets
 --
 testsZSets :: [Test]
-testsZSets = [testZAdd, testZRank, testZRemRange, testZRange, testZStore]
+testsZSets = [testZSets, testZStore]
 
-testZAdd :: Test
-testZAdd = testCase "zadd/zrem/zcard/zscore/zincrby" $ do
-    zadd "key" [(1,"v1"),(2,"v2"),(40,"v3")] >>=? 3
-    zscore "key" "v3"                        >>=? Just 40
-    zincrby "key" 2 "v3"                     >>=? 42
-    zrem "key" ["v3","notAKey"]              >>=? 1
-    zcard "key"                              >>=? 2
+testZSets :: Test
+testZSets = testCase "sorted sets" $ do
+    zadd "key" [(1,"v1"),(2,"v2"),(40,"v3")]          >>=? 3
+    zcard "key"                                       >>=? 3
+    zscore "key" "v3"                                 >>=? Just 40
+    zincrby "key" 2 "v3"                              >>=? 42
+                                                     
+    zrank "key" "v1"                                  >>=? Just 0
+    zrevrank "key" "v1"                               >>=? Just 2
+    zcount "key" 10 100                               >>=? 1
 
-testZRank :: Test
-testZRank = testCase "zrank/zrevrank/zcount" $ do
-    zadd "key" [(1,"v1"),(2,"v2"),(40,"v3")] >>=? 3
-    zrank "notAKey" "v1"                     >>=? Nothing
-    zrank "key" "v1"                         >>=? Just 0
-    zrevrank "key" "v1"                      >>=? Just 2
-    zcount "key" 10 100                      >>=? 1
-
-testZRemRange :: Test
-testZRemRange = testCase "zremrangebyscore/zremrangebyrank" $ do
-    zadd "key" [(1,"v1"),(2,"v2"),(40,"v3")] >>=? 3
-    zremrangebyrank "key" 0 1                >>=? 2
-    zadd "key" [(1,"v1"),(2,"v2"),(40,"v3")] >>=? 2
-    zremrangebyscore "key" 10 100            >>=? 1
-
-testZRange :: Test
-testZRange = testCase "zrange/zrevrange/zrangebyscore/zrevrangebyscore" $ do
-    zadd "key" [(1,"v1"),(2,"v2"),(3,"v3")]           >>=? 3
     zrange "key" 0 1                                  >>=? ["v1","v2"]
     zrevrange "key" 0 1                               >>=? ["v3","v2"]
     zrangeWithscores "key" 0 1                        >>=? [("v1",1),("v2",2)]
-    zrevrangeWithscores "key" 0 1                     >>=? [("v3",3),("v2",2)]
+    zrevrangeWithscores "key" 0 1                     >>=? [("v3",42),("v2",2)]
     zrangebyscore "key" 0.5 1.5                       >>=? ["v1"]
     zrangebyscoreWithscores "key" 0.5 1.5             >>=? [("v1",1)]
     zrangebyscoreLimit "key" 0.5 2.5 0 1              >>=? ["v1"]
@@ -504,6 +274,10 @@ testZRange = testCase "zrange/zrevrange/zrangebyscore/zrevrangebyscore" $ do
     zrevrangebyscoreWithscores "key" 1.5 0.5          >>=? [("v1",1)]
     zrevrangebyscoreLimit "key" 2.5 0.5 0 1           >>=? ["v2"]
     zrevrangebyscoreWithscoresLimit "key" 2.5 0.5 0 1 >>=? [("v2",2)]
+    
+    zrem "key" ["v2"]                                 >>=? 1
+    zremrangebyscore "key" 10 100                     >>=? 1
+    zremrangebyrank "key" 0 0                         >>=? 1
 
 testZStore :: Test
 testZStore = testCase "zunionstore/zinterstore" $ do
