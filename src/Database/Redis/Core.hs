informatikr/hedis

commit 1e9880928f07e57838a6120cd54aca817b806882

Enforce all replies being recieved in runRedis
Lazy I/O used for automatic pipelining should not leak from
runRedis calls. Otherwise it could lead to e.g. conflict with
resource pool scope seen in #23 or exceptions being thrown in
unexpected places (see e.g. #35).
To prevent this from happening now all replies up to the last
should be received inside of `runRedis`.

src/Database/Redis/Core.hs
@@ -12,7 +12,8 @@ module Database.Redis.Core (
 
 import Prelude
 import Control.Applicative
-import Control.Monad.Reader
+import Control.Exception (evaluate)
+import Control.Monad.State
 import qualified Data.ByteString as B
 import Data.Pool
 import Data.Time
@@ -32,7 +33,7 @@ import Database.Redis.Types
 --
 --  In this context, each result is wrapped in an 'Either' to account for the
 --  possibility of Redis returning an 'Error' reply.
-newtype Redis a = Redis (ReaderT (PP.Connection Reply) IO a)
+newtype Redis a = Redis (StateT (PP.Connection Reply, Maybe Reply) IO a)
     deriving (Monad, MonadIO, Functor, Applicative)
 
 -- |This class captures the following behaviour: In a context @m@, a command
@@ -64,15 +65,26 @@ runRedis (Conn pool) redis =
 -- |Internal version of 'runRedis' that does not depend on the 'Connection'
 --  abstraction. Used to run the AUTH command when connecting. 
 runRedisInternal :: PP.Connection Reply -> Redis a -> IO a
-runRedisInternal env (Redis redis) = runReaderT redis env
+runRedisInternal env (Redis redis) = do
+  (r, (_, lastReply)) <- runStateT redis (env, Nothing)
+  void $ traverse evaluate lastReply
+  return r
+
+
+getConn :: StateT (PP.Connection Reply, Maybe Reply) IO (PP.Connection Reply)
+getConn = fst <$> get
+
+
+putReply :: Reply -> StateT (PP.Connection Reply, Maybe Reply) IO ()
+putReply r = modify $ \(c, _) -> (c, Just r)
 
 
 recv :: (MonadRedis m) => m Reply
-recv = liftRedis $ Redis $ ask >>= liftIO . PP.recv
+recv = liftRedis $ Redis getConn >>= liftIO . PP.recv
 
 send :: (MonadRedis m) => [B.ByteString] -> m ()
 send req = liftRedis $ Redis $ do
-    conn <- ask
+    conn <- getConn
     liftIO $ PP.send conn (renderRequest req)
 
 -- |'sendRequest' can be used to implement commands from experimental
@@ -88,10 +100,12 @@ send req = liftRedis $ Redis $ do
 sendRequest :: (RedisCtx m f, RedisResult a)
     => [B.ByteString] -> m (f a)
 sendRequest req = do
-    r <- liftRedis $ Redis $ do
-        conn <- ask
-        liftIO $ PP.request conn (renderRequest req)
-    returnDecode r
+    r' <- liftRedis $ Redis $ do
+        conn <- getConn
+        r <- liftIO $ PP.request conn (renderRequest req)
+        putReply r
+        return r
+    returnDecode r'
 
 
 --------------------------------------------------------------------------------

src/Database/Redis.hs
@@ -101,8 +101,9 @@ module Database.Redis (
     --  Automatic pipelining makes use of Haskell's laziness. As long as a
     --  previous reply is not evaluated, subsequent commands can be pipelined.
     --
-    --  Automatic pipelining also works across several calls to 'runRedis', as
-    --  long as replies are only evaluated /outside/ the 'runRedis' block.
+    --  Automatic pipelining is limited to the scope of 'runRedis' call and
+    --  it is guaranteed that every reply expected as a part of 'runRedis'
+    --  execution gets received after 'runRedis` invocation.
     --
     --  To keep memory usage low, the number of requests \"in the pipeline\" is
     --  limited (per connection) to 1000. After that number, the next command is

commit 13619ea332ae6accc503c323b6c4702dbdf0f892

Major performance improvement
Fixes performace regression caused by 1e98809.
Single-threaded benchmark results with
nRequests = 1000000, nClients = 5.

Now:

ping                  106347.14 Req/s
get                    96889.61 Req/s
mget                   66234.41 Req/s
ping (pipelined)      803863.69 Req/s
multiExec get 1        53070.78 Req/s
multiExec get 50      452929.94 Req/s
multiExec get 1000    422652.84 Req/s

And previously:

ping                   95638.95 Req/s
get                    88974.55 Req/s
mget                   63976.22 Req/s
ping (pipelined)      745275.88 Req/s
multiExec get 1        48045.23 Req/s
multiExec get 50      432292.04 Req/s
multiExec get 1000    393029.70 Req/s

src/Database/Redis/Core.hs
@@ -15,8 +15,9 @@ import Prelude
 import Control.Applicative
 #endif
 import Control.Exception (evaluate)
-import Control.Monad.State
+import Control.Monad.Reader
 import qualified Data.ByteString as B
+import Data.IORef
 import Data.Pool
 import Data.Time
 import Network
@@ -38,8 +39,11 @@ import Database.Redis.Types
 --
 --  In this context, each result is wrapped in an 'Either' to account for the
 --  possibility of Redis returning an 'Error' reply.
-newtype Redis a = Redis (StateT (PP.Connection, Maybe Reply) IO a)
+newtype Redis a = Redis (ReaderT RedisEnv IO a)
     deriving (Monad, MonadIO, Functor, Applicative)
+
+data RedisEnv = Env { envConn :: PP.Connection, envLastReply :: IORef Reply }
+
 
 -- |This class captures the following behaviour: In a context @m@, a command
 --  will return it's result wrapped in a \"container\" of type @f@.
@@ -65,31 +69,34 @@ instance MonadRedis Redis where
 --  while all connections from the pool are in use.
 runRedis :: Connection -> Redis a -> IO a
 runRedis (Conn pool) redis =
-    withResource pool $ \conn -> runRedisInternal conn redis
+  withResource pool $ \conn -> runRedisInternal conn redis
 
 -- |Internal version of 'runRedis' that does not depend on the 'Connection'
 --  abstraction. Used to run the AUTH command when connecting.
 runRedisInternal :: PP.Connection -> Redis a -> IO a
-runRedisInternal env (Redis redis) = do
-  (r, (_, lastReply)) <- runStateT redis (env, Nothing)
-  void $ traverse evaluate lastReply
+runRedisInternal conn (Redis redis) = do
+  -- Dummy reply in case no request is sent.
+  ref <- newIORef (SingleLine "nobody will ever see this")
+  r <- runReaderT redis (Env conn ref)
+  -- Evaluate last reply to keep lazy IO inside runRedis.
+  readIORef ref >>= (`seq` return ())
   return r
 
-
-getConn :: StateT (PP.Connection, Maybe Reply) IO PP.Connection
-getConn = fst <$> get
-
-
-putReply :: Reply -> StateT (PP.Connection, Maybe Reply) IO ()
-putReply r = modify $ \(c, _) -> (c, Just r)
-
+setLastReply :: Reply -> ReaderT RedisEnv IO ()
+setLastReply r = do
+  ref <- asks envLastReply
+  lift (writeIORef ref r)
 
 recv :: (MonadRedis m) => m Reply
-recv = liftRedis $ Redis getConn >>= liftIO . PP.recv
+recv = liftRedis $ Redis $ do
+  conn <- asks envConn
+  r <- liftIO (PP.recv conn)
+  setLastReply r
+  return r
 
 send :: (MonadRedis m) => [B.ByteString] -> m ()
 send req = liftRedis $ Redis $ do
-    conn <- getConn
+    conn <- asks envConn
     liftIO $ PP.send conn (renderRequest req)
 
 -- |'sendRequest' can be used to implement commands from experimental
@@ -106,9 +113,9 @@ sendRequest :: (RedisCtx m f, RedisResult a)
     => [B.ByteString] -> m (f a)
 sendRequest req = do
     r' <- liftRedis $ Redis $ do
-        conn <- getConn
+        conn <- asks envConn
         r <- liftIO $ PP.request conn (renderRequest req)
-        putReply r
+        setLastReply r
         return r
     returnDecode r'
 