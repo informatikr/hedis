a86378633f77c050b4c41715134a5f305fcbcc0e
Alexander Vershilov <alexander.vershilov@sirius.online>
2023-06-10 17:00:32 +0200

    Fix keyToSlot function.
    keyToSlot function contained several bugs, so it didn't work according to the
    spec and the values didn't  match result of the CLUSTER KEYSLOT command.
    
    In this commit we fix those bugs:
    1. Now tags are properly matched;
    2. Non matching tags return key calculated on entire string;
    3. First matching tag is used.
    
    All the cases were tested with CLUSTER KEYSLOT command and added to the
    doctests.  Unfortunatly it's not added to the full featured testsuite
    because running CLUSTR KEYSLOT command requirs running redis cluster
    
    Fixes #203.

--- src/Database/Redis/Cluster/HashSlot.hs
+++ src/Database/Redis/Cluster/HashSlot.hs
@@ -5,7 +5,11 @@ module Database.Redis.Cluster.HashSlot(HashSlot, keyToSlot) where
 import Data.Bits((.&.), xor, shiftL)
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString as BS
+import Data.Maybe (fromMaybe)
 import Data.Word(Word8, Word16)
+
+-- $setup
+-- >>> :set -XOverloadedStrings
 
 newtype HashSlot = HashSlot Word16 deriving (Num, Eq, Ord, Real, Enum, Integral, Show)
 
@@ -13,16 +17,31 @@ numHashSlots :: Word16
 numHashSlots = 16384
 
 -- | Compute the hashslot associated with a key
+--
+-- >>> keyToSlot "123"
+-- HashSlot 5970
+-- >>> keyToSlot "{123"
+-- HashSlot 2872
+-- >>> keyToSlot "{123}"
+-- HashSlot 5970
+-- >>> keyToSlot "{}123"
+-- HashSlot 7640
+-- >>> keyToSlot "{123}1{abc}"
+-- HashSlot 5970
+-- >>> keyToSlot "\00\01"
+-- HashSlot 4129
 keyToSlot :: BS.ByteString -> HashSlot
 keyToSlot = HashSlot . (.&.) (numHashSlots - 1) . crc16 . findSubKey
 
 -- | Find the section of a key to compute the slot for.
 findSubKey :: BS.ByteString -> BS.ByteString
-findSubKey key = case Char8.break (=='{') key of
-  (whole, "") -> whole
-  (_, xs) -> case Char8.break (=='}') (Char8.tail xs) of
-    ("", _) -> key
-    (subKey, _) -> subKey
+findSubKey key = fromMaybe key (go key) where
+  go bs = case Char8.break (=='{') bs of
+    (_, "") -> Nothing
+    (_, xs)  -> case Char8.break (=='}') (Char8.tail xs) of
+      ("", _) -> go (Char8.tail xs)
+      (_, "") -> Nothing
+      (subKey, _) -> Just subKey
 
 crc16 :: BS.ByteString -> Word16
 crc16 = BS.foldl (crc16Update 0x1021) 0
