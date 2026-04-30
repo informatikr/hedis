1f1d207e95e18d2eb61eb0378590d37c398ad4fe
Alexander Vershilov <alexander.vershilov@gmail.com>
2026-05-01 00:01:35 +0200

    Fix tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
 import qualified Test.Framework as Test
@@ -8,15 +9,19 @@ import System.Environment
 
 main :: IO ()
 main = do
-    mhost <- lookupEnv "REDIS_HOST"
-    conn <- connect $ maybe id (\x -> \ci -> ci{connectHost=x}) mhost $ defaultConnectInfo
-    Test.defaultMain (tests conn)
+    host <- lookupEnv "REDIS_HOST" >>= \case
+        Just host -> return host
+        Nothing -> return "localhost"
+    conn <- connect defaultConnectInfo { connectHost = host }
+    Test.defaultMain (tests host conn)
 
-tests :: Connection -> [Test.Test]
-tests conn = map ($ conn) $ concat
+tests :: String -> Connection -> [Test.Test]
+tests host conn = map ($ conn) $ concat
     [ testsMisc, testsKeys, testsStrings, [testHashes], testsLists, testsSets, [testHyperLogLog]
     , testsZSets, [testPubSub], [testTransaction], [testScripting]
-    , testsConnection, testsClient, testsServer, [testScans, testSScan, testHScan, testZScan], [testZrangelex]
+    , testsConnection host
+    , testsClient, testsServer
+    , [testScans, testSScan, testHScan, testZScan], [testZrangelex]
     , [testXAddRead, testXReadGroup, testXRange, testXpending, testXClaim, testXInfo, testXDel, testXTrim]
     , testPubSubThreaded
       -- should always be run last as connection gets closed after it
@@ -32,9 +37,17 @@ testsServer =
     [testServer, testBgrewriteaof, testFlushall, testInfo, testConfig
     ,testSlowlog, testDebugObject]
 
-testsConnection :: [Test]
-testsConnection = [ testConnectAuth, testConnectAuthUnexpected, testConnectAuthAcl,testConnectDb
-                  , testConnectDbUnexisting, testEcho, testPing, testSelect ]
+testsConnection :: String -> [Test]
+testsConnection host =
+    [ testConnectAuth host
+    , testConnectAuthUnexpected host
+    , testConnectAuthAcl host
+    , testConnectDb host
+    , testConnectDbUnexisting host
+    , testEcho
+    , testPing
+    , testSelect
+    ]
 
 testsKeys :: [Test]
 testsKeys = [ testKeys, testKeysNoncluster, testExpireAt, testSort, testGetType, testObject ]
--- test/MainRedis7.hs
+++ test/MainRedis7.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
 import qualified Test.Framework as Test
 import Database.Redis
+import System.Environment (lookupEnv)
 import Tests
 
 main :: IO ()
 main = do
-    conn <- connect defaultConnectInfo
+    host <- lookupEnv "REDIS_HOST" >>= \case
+        Just host -> return host
+        Nothing -> return "localhost"
+    conn <- connect defaultConnectInfo{ connectHost = host }
     Test.defaultMain (tests conn)
 
 tests :: Connection -> [Test.Test]
